864b28a016a7cf78a6762c25e6d2f5e52252c9f3
oalexallan <allexallansilveira@gmail.com>
2023-12-29 18:10:10 -0300

    git add .
    git commit -m "Update default"
    
    git push -u origin main

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,3 @@
-function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
-
 document.addEventListener('DOMContentLoaded', function () {
     const obterConselhoBtn = document.getElementById('obterConselho');
     const numeroConselhoSpan = document.getElementById('numeroConselho');
@@ -19,26 +12,30 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     async function traduzir(texto) {
-        const url = `https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=en&to=${language}`;
-        const headers = {
-            'Ocp-Apim-Subscription-Key': 'd7d183a0e2c74f0682149639e89e9bae',
-            'Ocp-Apim-Subscription-Region': 'brazilsouth',
-            'Content-type': 'application/json',
-            'X-ClientTraceId': uuid.v4()
-        };        
-        const body = [{ 'Text': texto }];
+        if (language === 'en') {
+            return texto;
+        } else {
+            const url = `https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=en&to=${language}`;
+            const headers = {
+                'Ocp-Apim-Subscription-Key': 'd7d183a0e2c74f0682149639e89e9bae',
+                'Ocp-Apim-Subscription-Region': 'brazilsouth',
+                'Content-type': 'application/json',
+                'X-ClientTraceId': uuidv4()
+            };        
+            const body = [{ 'Text': texto }];
 
-        try {
-            const response = await axios({
-                baseURL: url,
-                method: 'post',
-                headers: headers,
-                data: body,
-                responseType: 'json'
-            });
-            return response.data[0].translations[0].text;
-        } catch (error) {
-            console.error(error);
+            try {
+                const response = await axios({
+                    baseURL: url,
+                    method: 'post',
+                    headers: headers,
+                    data: body,
+                    responseType: 'json'
+                });
+                return response.data[0].translations[0].text;
+            } catch (error) {
+                console.error(error);
+            }
         }
     }
 
