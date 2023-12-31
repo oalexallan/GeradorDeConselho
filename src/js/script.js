608bbe7f5656f6ab971b831e120a462da9d91ca2
oalexallan <allexallansilveira@gmail.com>
2023-12-31 02:09:14 -0300

    remove

--- src/js/script.js
+++ src/js/script.js
@@ -11,6 +11,34 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
+    async function traduzir(texto) {
+        if (language === 'en') {
+            return texto;
+        } else {
+            const url = `https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=en&to=${language}`;
+            const headers = {
+                'Ocp-Apim-Subscription-Key': 'd7d183a0e2c74f0682149639e89e9bae',
+                'Ocp-Apim-Subscription-Region': 'brazilsouth',
+                'Content-type': 'application/json',
+                'X-ClientTraceId': uuid.v4()
+            };        
+            const body = [{ 'Text': texto }];
+
+            try {
+                const response = await axios({
+                    baseURL: url,
+                    method: 'post',
+                    headers: headers,
+                    data: body,
+                    responseType: 'json'
+                });
+                return response.data[0].translations[0].text;
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
+
     obterConselhoBtn.addEventListener('click', async function () {
         obterConselhoBtn.disabled = true;
         exibirConselho.innerHTML = '<img src="./src/img/loading.gif" alt="Loading..." width="50" height="50">';
@@ -19,8 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const conselhoData = await conselhoResponse.json();
         const conselho = conselhoData.slip.advice;
 
-        const conselhoTraduzidoResponse = await fetch(`/api/conselho?texto=${encodeURIComponent(conselho)}&language=${encodeURIComponent(language)}`);
-        const conselhoTraduzido = await conselhoTraduzidoResponse.text();
+        const conselhoTraduzido = await traduzir(conselho);
 
         numeroConselhoSpan.textContent = conselhoData.slip.id;
         exibirConselho.textContent = `"${conselhoTraduzido}"`;
